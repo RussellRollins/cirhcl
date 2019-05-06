b7cdfcc633bb9269d08865eec154b5082b73c683
Russell Rollins <russell1124@gmail.com>
2019-05-06 17:31:58 -0400

    Adds primitive main.go that actually converts files.

--- cmd/cirhcl/main.go
+++ cmd/cirhcl/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"os"
+	"path/filepath"
+	"strings"
 
 	"github.com/pkg/errors"
 
@@ -10,9 +12,36 @@ import (
 )
 
 func main() {
-	fmt.Println("Hello, World!")
-	convert.Convert(os.Stdin, "foo", os.Stdout)
+	if err := run(); err != nil {
+		fmt.Println(errors.Wrap(err, "error running cirhcl"))
+		os.Exit(1)
+	}
+}
 
-	err := errors.Errorf("Error: %d", 3)
-	fmt.Println(err)
+func run() error {
+	if len(os.Args) < 2 {
+		return errors.New("insufficient command line arguments, please specify the file to be converted")
+	}
+	if len(os.Args) > 2 {
+		return errors.Errorf("unexpected command line arguments [%s]", strings.Join(os.Args[2:], " "))
+	}
+
+	name := os.Args[1]
+	input, err := os.Open(name)
+	if err != nil {
+		return errors.Wrapf(err, "unable to open %s", name)
+	}
+	defer input.Close()
+
+	noExtension := strings.TrimSuffix(input.Name(), filepath.Ext(input.Name()))
+	outName := fmt.Sprintf("%s.yaml", noExtension)
+
+	output, err := os.Create(outName)
+	if err != nil {
+		return errors.Wrapf(err, "unable to open %s", outName)
+	}
+	defer output.Close()
+
+	return convert.Convert(input, name, output)
+
 }
